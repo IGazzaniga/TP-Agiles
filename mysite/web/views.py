e15b6d8f3fcd14def4526260438d57dc07728767
StegerI <stegerivan95@gmail.com>
2017-09-29 14:37:32 -0300

    Se agregan los estilos, archivos js, se configura la view y las urls

--- mysite/web/views.py
+++ mysite/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def web_view(request):
+    """Returns the website."""
+    return render(request, 'web/index.html', {})
