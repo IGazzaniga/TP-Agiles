60439c28caf8f8c6debffbc9ea5526345b8a71bf
IGazzaniga <ignagazzaniga@hotmail.com.ar>
2017-11-25 18:23:37 -0300

    Modificacion de estilos

--- mysite/web/views.py
+++ mysite/web/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.utils import timezone
-from .models import Novedad
+from .models import Novedad, Trabajo, Investigacion
 
 # Create your views here.
 def web_view(request):
     """Returns the website."""
     novedades = Novedad.objects.all().order_by('-fechaP')[:5]
+    trabajos = Trabajo.objects.all().order_by('-fechaP')
     return render(request, 'web/index.html', {'novedades': novedades})
 
    
