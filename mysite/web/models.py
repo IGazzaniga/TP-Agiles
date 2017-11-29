c0fa2ec11cf85e62dad44f69c0ca13d2625db96c
IGazzaniga <ignagazzaniga@hotmail.com.ar>
2017-11-29 19:39:13 -0300

    LISTOOOOOOOO(ponele)

--- mysite/web/models.py
+++ mysite/web/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Novedad(models.Model):
     """Una novedad"""
-    titulo = models.CharField(max_length=30) #Titulo de la novedad
+    titulo = models.CharField(max_length=20) #Titulo de la novedad
     contenido = models.TextField() #Contenido de la novedad
     fechaP = models.DateTimeField('fecha de publicacion')
     tags = models.CharField(max_length=30)
--- mysite/web/static/web/js/viewBiblio.js
+++ mysite/web/static/web/js/viewBiblio.js
@@ -1,14 +1,43 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-
+var j;
+acc[0].nextElementSibling.style.maxHeight = acc[0].nextElementSibling.scrollHeight + "px";
 for (i = 0; i < acc.length; i++) {
   acc[i].onclick = function() {
+    
+    for (j = 0; j < acc.length; j++){
+      var pepe = acc[j].nextElementSibling;
+      pepe.style.maxHeight = null;
+    }
+    
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+
+var acc1 = document.getElementsByClassName("accordeon");
+var i1;
+var j1;
+acc1[0].nextElementSibling.style.maxHeight = acc1[0].nextElementSibling.scrollHeight + "px";
+for (i1 = 0; i1 < acc1.length; i1++) {
+  acc1[i1].onclick = function() {
+    
+    for (j1 = 0; j1 < acc1.length; j1++){
+      var pepe1 = acc1[j1].nextElementSibling;
+      pepe1.style.maxHeight = null;
+    }
+    
+    this.classList.toggle("active");
+    var panel1 = this.nextElementSibling;
+    if (panel1.style.maxHeight){
+      panel1.style.maxHeight = null;
+    } else {
+      panel1.style.maxHeight = panel1.scrollHeight + "px";
     } 
   }
 }
